4dbcf2a4e61921ffc458ca6be56cfc7a1e9b7368
thienht1997 <thienht1997tt@gmail.com>
2019-09-20 14:34:24 +0700

    add progress bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NameCardComponent } from './name-card/name-card.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NameCardComponent],
+  declarations: [ AppComponent, HelloComponent, NameCardComponent,
+  ProgressBarComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
